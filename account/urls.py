9e28e762d53358371e1ae1a92f3b60a7a3d1b19b
MohammadReza0044 <mohammadreza.shayegan@gmail.com>
2023-03-30 18:47:08 +0430

    proflie is done.

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,7 @@ app_name = 'Account'
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.LogoutView.as_view(), name="logout"),
-#     path("password_change/", views.PasswordChangeView.as_view(), name="password_change"),
-#     path("password_change/done/",views.PasswordChangeDoneView.as_view(),name="password_change_done"),
+    path("password_change/done/",views.PasswordChangeDoneView.as_view(),name="password_change_done"),
 #     path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
 #     path("password_reset/done/",views.PasswordResetDoneView.as_view(),name="password_reset_done"),
 #     path("reset/<uidb64>/<token>/",views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
@@ -16,8 +15,15 @@ urlpatterns = [
 ]
 
 
+from account import views
 
+app_name = 'Account'
 
+urlpatterns += [
+    path("password-change", views.PasswordChange.as_view(), name="password_change"),
+    path("profile", views.UserProfile.as_view(), name="profile"),
+    path("profile/user-info", views.UserInfo.as_view(), name="user_info"),
 
+]
 
 
--- account/views.py
+++ account/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render , get_object_or_404
-from django.http.response import HttpResponseRedirect
+from django.shortcuts import render 
 from django.urls import reverse_lazy
-from django.views.generic import ListView , CreateView , UpdateView , DeleteView, View
-from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.views.generic import ListView, UpdateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin 
-from django.db.models import Q
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.models import User
 
 
 
+class UserProfile(LoginRequiredMixin,TemplateView):
+    template_name = 'registration/profile.html'
 
 
+
+class UserInfo(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
+    model = User
+    fields = ['first_name','last_name','email','username']
+    template_name = 'registration/user_info.html'
+    success_url = reverse_lazy ('Account:user_info')
+    success_message = "کاربر، با موفقیت ویرایش شد."
+
+    def get_object(self):
+        return User.objects.get(pk = self.request.user.pk) 
+
+
+
+class PasswordChange(LoginRequiredMixin,PasswordChangeView):
+    success_url = reverse_lazy('Account:password_change_done')
